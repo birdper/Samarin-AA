1fe364f35496c57cce2fa5a067bf4a663bbef4eb
birdper <h.garo@yandex.ru>
2024-02-12 02:15:54 +0300

    Add nav graph

--- app/src/main/java/org/birdper/movies/presentation/MainActivity.kt
+++ app/src/main/java/org/birdper/movies/presentation/MainActivity.kt
@@ -1,12 +1,18 @@
-package org.birdper.films
+package org.birdper.movies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import org.birdper.movies.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)
 
         setContentView(R.layout.activity_main)
     }
